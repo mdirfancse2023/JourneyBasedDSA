1cc7541e8e112fb32663c1de0dfe1bf4251e7200
Md Irfan <82992328+mdirfancse2023@users.noreply.github.com>
2024-04-20 19:40:02 +0000

    1.0 ArrayDSA

--- ArraysDSA/Array.java
+++ ArraysDSA/Array.java
@@ -2,7 +2,36 @@ import java.util.*;
 class Array{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        myArray obj = new myArray();
+        int arr[] = new int[10];
+        int ele = sc.nextInt();
+        int pos = sc.nextInt();
+        obj.insert(arr,ele,pos);
+        obj.delete(arr,pos);
+        obj.update(arr,pos,ele);
+        obj.display(arr);
         sc.close();
         
+    }
+}
+class myArray{
+    int size = 0;
+    void insert(int arr[],int ele,int pos){
+        if(size<arr.length){
+            arr[pos-1] = ele;
+            size++;
+        }
+    }
+    void delete(int arr[],int pos){
+        arr[pos-1] = 0;
+        size--;
+    }
+    void update(int arr[],int pos,int ele){
+        arr[pos-1] = ele;
+    }
+    void display(int arr[]){
+        for (int e : arr) {
+            System.out.println(e);
+        }
     }
 }
