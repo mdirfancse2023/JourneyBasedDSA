ee7fcab618c53fddb9cb2a4cd2ee6eb35631be04
Md Irfan <82992328+mdirfancse2023@users.noreply.github.com>
2024-04-21 12:54:57 +0000

    1.2 StringFunc

--- StringDSA/Stringfunc.java
+++ StringDSA/Stringfunc.java
@@ -1,5 +1,18 @@
 package StringDSA;
-
 public class Stringfunc {
-    
+   public static void main(String[] args) {
+    String str = "   The Journey Based DSA    ";
+    System.out.println(str.length());
+    System.out.println(str.charAt(0));
+    System.out.println(str.substring(1,5));
+    System.out.println(str.indexOf('e'));
+    System.out.println(str.toLowerCase());
+    System.out.println(str.toUpperCase());
+    System.out.println(str.trim());
+    System.out.println(str.replace('e','m'));
+
+    for (char st : str.toCharArray()) {
+        System.out.println(st);
+    }
+   } 
 }
